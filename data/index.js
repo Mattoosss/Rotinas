1d066fd72eddd90beb730ea2d64e942cd72039a2
Your Name <you@example.com>
2024-04-09 14:23:14 -0300

    oi

---
+++ data/index.js
@@ -0,0 +1,47 @@
+const DATA =
+[
+    {
+    id:'01',
+    porcentagem:'45%',
+    titulo:'cafe da manha',
+    imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeap9FsnMb1AS6oJwke-t9zMfioIy0jWoDcA&s',
+    tex:'Eu leio durante a semana',
+},
+{
+    id:'02',
+    porcentagem:'45%',
+    titulo:'trabalho',
+    imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKXux-AXH6BClVOSYzKWNiCZzX3-AL3U8n9ULTDEuwyw&s',
+    descricao:'Eu leio durante a semana',
+},
+{
+    id:'03',
+    porcentagem:'45%',
+    titulo:'cafe da tarde',
+    imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6NjD9s7n6k3vAu4wcw4Qscq5KhqV3-NnhCZBwK41Chw&sEstudos',
+    descricao:'Eu leio durante a semana',
+},
+{
+    id:'04',
+    porcentagem:'45%',
+    titulo:'Estudos',
+    imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSds1q7vReGqfwNW1cSpNIEoFsvtUOVmzj0lGR5WAxvNA&s',
+    descricao:'Eu leio durante a semana',
+},
+{
+    id:'05',
+    porcentagem:'45%',
+    titulo:'lazer',
+    imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9exVE2iU6BX5CvRSdXk-mHV9WI-aVg9ax-7HWjv-eCw&s',
+    descricao:'Eu leio durante a semana',
+},
+{
+    id:'06',
+    porcentagem:'45%',
+    titulo:'descanço',
+    imagem:'https://authent.com.br/wp-content/uploads/sites/21/2022/06/a-importancia-do-equilibrio-entre-trabalho-e-descanso-para-aumentar-a-produtividade-1200x542.png',
+    descricao:'Eu leio durante a semana',
+}
+];
+
+export default DATA;
